b4424f30685b9e0f97f838f74f0a479664ddb5d2
sergeybwk <92894683+sergeybwk@users.noreply.github.com>
2024-09-26 15:56:36 +0300

    init commit

---
+++ lib/src/domain/models/character.g.dart
@@ -0,0 +1,21 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'character.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+Character _$CharacterFromJson(Map<String, dynamic> json) => Character(
+      json['name'] as String,
+      json['image'] as String,
+      json['status'] as String,
+      json['species'] as String,
+    );
+
+Map<String, dynamic> _$CharacterToJson(Character instance) => <String, dynamic>{
+      'name': instance.name,
+      'image': instance.image,
+      'status': instance.status,
+      'species': instance.species,
+    };
